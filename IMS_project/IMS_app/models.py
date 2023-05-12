573890753afbaac9825dc9b685ef56a6f68d39f5
852hamza <hamzaarain852sba@gmail.com>
2023-05-13 02:21:22 +0500

    add label in model.py

--- IMS_project/IMS_app/models.py
+++ IMS_project/IMS_app/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Department(models.Model):
     name = models.CharField(max_length=50)
+    
+    class Meta:
+        app_label = 'IMS_app'
 
 class EmployeeDetail(models.Model):
     employee_code = models.CharField(primary_key=True, max_length=10)
@@ -13,14 +16,21 @@ class EmployeeDetail(models.Model):
     age = models.PositiveSmallIntegerField()
     department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='employees')
 
+    class Meta:
+        app_label = 'IMS_app'
 class Product(models.Model):
     product_tag = models.CharField(primary_key=True, max_length=10)
     product_type = models.CharField(max_length=50)
     quantity = models.PositiveIntegerField()
     purchase_date = models.DateField()
 
+    class Meta:
+        app_label = 'IMS_app'
 class EmployeeProduct(models.Model):
     employee_detail = models.ForeignKey(EmployeeDetail, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     date = models.DateField()
+
+    class Meta:
+        app_label = 'IMS_app'
