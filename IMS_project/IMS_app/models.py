87d82fd304c96ab8ae6c8657edf9713316d30f88
852hamza <hamzaarain852sba@gmail.com>
2023-05-13 11:45:31 +0500

    add employee_code at user view

--- IMS_project/IMS_app/models.py
+++ IMS_project/IMS_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Department(models.Model):
     name = models.CharField(max_length=50)
     
+    def __str__(self):
+        return self.name 
     class Meta:
         app_label = 'IMS_app'
 
